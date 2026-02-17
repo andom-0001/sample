5ff55cbb0b6bb430dc65b159f9c396e8e1e41d3e
andom-0001 <anandkumar241103@gmail.com>
2026-02-17 09:10:59 +0530

    sample

--- logdata.js
+++ logdata.js
@@ -3,19 +3,34 @@ const fs = require('fs');
 const port = 8080;
 
 const server = http.createServer((req, res) => {
+
     const now = new Date().toLocaleString();
-    const logMessage = `{now} ${req.url}; New Request Receive`;
+    const logMessage = `${now} ${req.url}; New Request Received`;
 
     console.log(logMessage);
+    fs.appendFile("request_log.txt", logMessage + "\n", (err) => {
+        if (err) console.log(err);
+    });
+
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Home Page");
+    }
+    else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("About Page");
+    }
+    else if (req.url === "/contact") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Contact Page");
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Page Not Found");
+    }
+
 });
 
-
-
-fs.appendFile("request_log.txt", logMessage + "\n", (err) => {
-    if (err) console.log(err);
-
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-if (req.url === "/"){
-    res.writeHead(200, { "content-type": "text/html"})
-}
